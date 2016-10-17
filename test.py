803f7f177e518c1d08d7429214ab1ae59c94f800
CaptWang <michaelwangtd@gmail.com>
2016-10-17 18:30:50 +0800

    lasted update

--- test.py
+++ test.py
@@ -5,9 +5,63 @@ from newseed.util import linkList
 import math
 import re
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import os
+
+
+filePath = os.path.join(os.path.dirname(__file__),'test.xls')
+wb = Workbook()
+sheet = wb.active
+sheet.append(['Title','Time','Type','Money','productCompany','productCompanyLink','investCompany','investCompanyLink','investIntroduce'])
+wb.save(filePath)
 
 
 
+
+
+# testList = ['','']
+# if testList:
+#     print('lll')
+
+
+
+# string = """
+# <p></p>
+# <p>深圳市银桦投资管理有限公司投资雷士光电科技有限公司。</p>
+# <p></p>"""
+# singleContentList = []
+# cleanedContentList = []
+# contentList = crawl.extractContentFromHtmlString(string)
+# print(contentList)
+#
+# for item in contentList:
+#     if item :
+#         singleContentList.append(item)
+# print(singleContentList)
+#
+# for item in singleContentList:
+#     temp = item.replace('\s','').replace('\t','').replace('\r','')
+#     cleanedContentList.append(temp)
+# print(cleanedContentList  )
+
+
+# string = """<p> TMT </p>
+# <p class="keyword">
+# <span class="btn red">企</span>
+# <a class="btn default" href="/company/2746" target="_blank">雷士光电科技有限公司 </a>
+# <span class="btn blue">投</span>
+# <a class="default" href="/vc/22210" target="_blank">银桦投资</a>
+# </p>
+# <p></p>
+# <p>深圳市银桦投资管理有限公司投资雷士光电科技有限公司。</p>
+# <p></p>"""
+# # print(re.findall('keyword">\s*.*?\s*</p>\s*(.*?)\s*$',string,re.S))     #['<p></p>\n<p>深圳市银桦投资管理有限公司投资雷士光电科技有限公司。</p>\n<p></p>']
+# print(re.findall('keyword">\s*.*?\s*</p>\s*(.*?)\s*',string,re.S))      #['']
+
+
+# print(type('sss'))
+# if type('sss') == str:
+#     print('类型为字符串...')
 
 
 # html = """<div class="info">
